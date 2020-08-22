0cabd08a99fc8ad2b37684ba24deb9d40f3b3044
JonathanGamaS <jonathan_gama_@hotmail.com>
2020-08-22 20:12:59 -0300

    Criacao das funcoes que geram token e a funcao que gera a pesquisa de usuario

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask_api import FlaskAPI
-from orchestration import key_generator
+from orchestration import key_generator, pesquisa_usuario
 
 app = FlaskAPI(__name__)
 
 
 @app.route('/api_key/')
 def generate_api_key():
-    return 
+    credentials = key_generator()
+    return credentials
+
+
+@app.route('/pesquisa_usuario/{termo_pesquisa}')
+def pesquisa_usuario_escavador(termo_pesquisa):
+    result = pesquisa_usuario(termo_pesquisa)
+    return result
--- orchestration.py
+++ orchestration.py
@@ -4,16 +4,46 @@ import json
 
 
 def key_generator():
-    email = os.getenv("email")
-    senha = os.getenv("password")
-    url = 'https://api.escavador.com/api/v1/request-token'
-    request_body = {
-        'username': email,  
-        'password': senha 
+    try:
+        email = os.getenv("email")
+        senha = os.getenv("password")
+        url = 'https://api.escavador.com/api/v1/request-token'
+        request_body = {
+            'username': email,  
+            'password': senha 
+        }
+        headers = {
+            'X-Requested-With': 'XMLHttpRequest',
+            'Content-Type': 'application/json'
+        }
+        response = requests.post(url=url, data=json.dumps(request_body), headers=headers)
+        response_dict = json.loads(response.content)
+        dados_credenciais = {
+            "token_type": response_dict["token_type"],
+            "access_token": response_dict["access_token"],
+            "refresh_token": response_dict["refresh_token"]
+        }
+        return dados_credenciais
+    except Exception as e:
+        raise e
+
+
+def pesquisa_usuario(termo_pesquisa):
+    credenciais = key_generator()
+    access_token = credenciais["access_token"]
+    url = 'https://api.escavador.com/api/v1/busca'
+
+    params = {
+        'q': termo_pesquisa,  
+        'qo': 't',  
+        'page': '1'
     }
+
     headers = {
-        'X-Requested-With': 'XMLHttpRequest',
-        'Content-Type': 'application/json'
+        'Authorization': f'Bearer {access_token}',
+        'X-Requested-With': 'XMLHttpRequest'
     }
-    response = requests.request('POST', url, headers=headers, json=payload)
-    response_dict = json.loads(response.text)
+
+    response = requests.get(url, headers=headers, params=params)
+    resopnse_dict = json.loads(response.content)
+    return resopnse_dict
